19deb87150d67425be963c02a0750a7fc014e5f1
Peter Parada <parad.peter@gmail.com>
2017-05-08 01:20:46 +0200

    Rename thread to train function

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const R = require('ramda')
 const Promise = require('bluebird')
 
-function thread (initialValue, ...list) {
+function train (initialValue, ...list) {
   return R.flatten(list)
     .reduce((acc, fn) => acc.then(fn), Promise.resolve(initialValue))
 }
 
-module.exports = thread
+module.exports = train
