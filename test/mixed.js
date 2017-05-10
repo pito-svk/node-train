a336dad74cf647eb067442d7f7c70ede6cf39758
Peter Parada <parad.peter@gmail.com>
2017-05-10 17:06:22 +0200

    Add test case with synchronous function in the middle

--- test/mixed.js
+++ test/mixed.js
@@ -13,3 +13,26 @@ test('First function is synchronous other async', async t => {
 
   t.is(actual, expected, 'Resulted value is 6')
 })
+
+test('Synchronous function is in the middle', async t => {
+  const removeAttr = attr => obj => {
+    delete obj[attr]
+    return obj
+  }
+  const addAttr = (attr, value) => obj => {
+    obj[attr] = value
+    return obj
+  }
+  const getKey = key => obj => obj[key]
+
+  const addAttrAfter50ms = (attr, value) => obj => new Promise(resolve => setTimeout(() => resolve(addAttr(attr, value)(obj)), 50))
+  const getKeyAfter50ms = key => obj => new Promise(resolve => setTimeout(() => resolve(getKey(key)(obj)), 50))
+
+  const actual = await train({ x: [1, 2, 3], y: 'foo' },
+                             addAttrAfter50ms('z', 4), // { x: [1, 2, 3], y: 'foo', z: 4 }
+                             removeAttr('x'), // { y: 'foo', z: 4 }
+                             getKeyAfter50ms('z')) // 'foo'
+  const expected = 4
+
+  t.is(actual, expected, 'Resulted value is 4')
+})
