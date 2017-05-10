7e7b08274a1a7b3397a4ca849e3c64587b126c0e
Peter Parada <parad.peter@gmail.com>
2017-05-10 23:07:42 +0200

    Add miscellaneous test cases

---
+++ test/miscellaneous.js
@@ -0,0 +1,23 @@
+const test = require('ava')
+const train = require('../train')
+
+test('Train without any arguments', t => {
+  const actual = train()
+  const expected = undefined
+
+  t.is(actual, expected, 'Resulted value is undefined')
+})
+
+test('Train with undefined', t => {
+  const actual = train(undefined)
+  const expected = undefined
+
+  t.is(actual, expected, 'Resulted value is undefined')
+})
+
+test('Train with null', t => {
+  const actual = train(null)
+  const expected = null
+
+  t.is(actual, expected, 'Resulted value is null')
+})
